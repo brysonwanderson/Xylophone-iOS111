f270057181b803724ed43d59cbf83a4023219465
Bryson Anderson <bryson.wanderson@gmail.com>
2018-07-27 22:36:17 -0600

    Sounds to Buttons

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -19,31 +19,47 @@ class ViewController: UIViewController{
         super.viewDidLoad()
     }
 
-
-
     @IBAction func notePressed(_ sender: UIButton) {
         print(sender.tag)
+       
         
-        let url = Bundle.main.url(forResource: "note1", withExtension: "wav")!
+        switch sender.tag {
+            case 1:
+                playNote(noteValue: "note1")
+            case 2:
+                playNote(noteValue: "note2")
+            case 3:
+                playNote(noteValue: "note3")
+            case 4:
+                playNote(noteValue: "note4")
+            case 5:
+                playNote(noteValue: "note5")
+            case 6:
+                playNote(noteValue: "note6")
+            case 7:
+                playNote(noteValue: "note7")
+            default:
+                print("This is an Error")   
+        }
+        
+    }
+    
+    func playNote(noteValue: String ) {
+        let url = Bundle.main.url(forResource: noteValue, withExtension: "wav")!
         
         do {
             player = try AVAudioPlayer(contentsOf: url)
             guard let player = player else { return }
-        
-        player.prepareToPlay()
-        player.play()
-        }  catch _ as Error {
-        
-        
-        }
-                
-            }
             
-        
-
-
+            player.prepareToPlay()
+            player.play()
+        }
+        catch {error as Error}; do {
+            print(Error.self)
+        }
+    }
     
-  
-
+    
+    
 }
 
